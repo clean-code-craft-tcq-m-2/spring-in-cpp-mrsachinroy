64bb9bd8a91579e9ccaadadef07d269f177cdb34
SACHIN ROY <sachinkumarsnp@gmail.com>
2022-10-21 14:57:39 +0530

    issue resolved
    update

--- stats.h
+++ stats.h
@@ -1,5 +1,51 @@
 #include <vector>
 
-namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+
+void glowLed();
+void sendmail();
+
+
+class IAlerter
+{
+
+};
+
+
+class EmailAlert : public IAlerter
+{
+public:
+    void (*emailSent)() =  &sendmail;
+};
+
+
+class LEDAlert : public IAlerter
+{
+public:
+    void (*ledGlows)() =  &glowLed;
+};
+
+class StatsAlerter
+{
+     
+public:
+    StatsAlerter(const float, std::vector<IAlerter *>)
+    {
+
+    }
+
+    void checkAndAlert(const std::vector<float>& )
+    {
+    }
+};
+
+struct Stats
+{
+    float average;
+    float max;
+    float min;
+} Stats;
+
+namespace Statistics
+{
+    struct Stats ComputeStatistics(const std::vector<float> &);
 }
